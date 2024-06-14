4c0068aada2accb40e71cae91297934b25012d3b
Gabriele Piazza <gabrielempiazza@gmail.com>
2024-06-14 23:03:43 +0100

    Made changes to the potential suppliers script before ownloading everything

--- 01_dataprep_C.R
+++ 01_dataprep_C.R
@@ -90,10 +90,10 @@ matched_potential_suppliers_orbis_file <- "matched_potential_suppliers_orbis_dat
 potential_suppliers_registration_file <- "22_10_31_potential_suppliers.csv"
 
 
-supplier_patent_lookup_dir <- paste0(data_proc_dir, "suppliers_patent_lookup")
-incoporation_size_nace_dir <- paste0(data_proc_dir, "Incorporation_size_nace_activity_lookup")
-incorporation_suppliers_orbis_dir <- paste0(data_raw_dir, "Orbis_size_classification_incorp_suppliers/")
-incorporation_suppliers_file <- list.files(incorporation_suppliers_orbis_dir, pattern = "xlsx", full.names = TRUE)
+pot_supplier_patent_lookup_dir <- paste0(data_proc_dir, "pot_suppliers_patent_lookup")
+incorporation_size_nace_lookup_pot_dir <- paste0(data_proc_dir, "potential_incorporation_size_nace_activity_lookup")
+incorporation_pot_suppliers_orbis_dir <- paste0(data_raw_dir, "Orbis_size_classification_incorp_pot_suppliers/")
+#incorporation_suppliers_file <- list.files(incorporation_suppliers_orbis_dir, pattern = "xlsx", full.names = TRUE)
 potential_suppliers_tech_balance_file <-"potential_suppliers_tech_balance"
 # suppliers_registration_file <- "suppliers_registration_year.csv"
 
@@ -101,7 +101,7 @@ potential_suppliers_tech_balance_file <-"potential_suppliers_tech_balance"
 
 
 ### Registration
-potential_registration <- readRDS(paste0(data_proc_dir, potential_suppliers_tech_balance_file)) %>% 
+potential_suppliers_tech_balance <- readRDS(paste0(data_proc_dir, potential_suppliers_tech_balance_file)) %>% 
   clean_names() %>% 
   rename(company_name = suppliername)
 
@@ -109,15 +109,15 @@ potential_registration <- readRDS(paste0(data_proc_dir, potential_suppliers_tech
 matched_potential_suppliers_orbis_data<- readRDS(paste0(data_proc_dir, matched_potential_suppliers_orbis_file))
 
 #Incorporation
-incorporation_nace_size_list <- lapply(incorporation_suppliers_file,read_results_sheet)
-incorporation_nace_size_data<- do.call(rbind, incorporation_nace_size_list)
-incorporation_nace_size_data<- incorporation_nace_size_data %>% select(-'...1') %>%
-  clean_names() %>% 
-  rename(bvd_id_number = bv_d_id_number) %>% 
+#incorporation_nace_size_list <- lapply(incorporation_suppliers_file,read_results_sheet)
+#incorporation_nace_size_data<- do.call(rbind, incorporation_nace_size_list)
+#incorporation_nace_size_data<- incorporation_nace_size_data %>% select(-'...1') %>%
+#  clean_names() %>% 
+ # rename(bvd_id_number = bv_d_id_number) %>% 
   # Identify and process year-only entries
-  mutate(
-    date_of_incorporation = as.character(date_of_incorporation),
-    incorporation_year = sapply(date_of_incorporation, convert_to_year))
+  #mutate(
+   # date_of_incorporation = as.character(date_of_incorporation),
+    #incorporation_year = sapply(date_of_incorporation, convert_to_year))
 
 # 3. Data Manipulation  ---------------------------------------------------
 
@@ -142,13 +142,13 @@ matched_potential_suppliers_orbis_data<- matched_potential_suppliers_orbis_data 
 bvd_id_lookup <- matched_potential_suppliers_orbis_data %>% 
   select(bvd_id_number, company_name) %>% distinct()# I need this to fill the missing year registration
 
-# I merge the new file with the bvd id lookup
-potential_suppliers_tech_balance<- potential_suppliers_tech_balance %>% 
-  rename(company_name = suppliername) %>% 
-  left_join(bvd_id_lookup)
+# I merge the new file with the bvd id lookup !
+ potential_suppliers_tech_balance<- potential_suppliers_tech_balance %>%
+   left_join(bvd_id_lookup)
 
 # There are some NAs for registration year 
 potential_suppliers_tech_balance <- potential_suppliers_tech_balance %>%
+  filter(!is.na(bvd_id_number)) %>% 
   group_by(bvd_id_number) %>%   # Group the data by 'bvd_id_number'
   rename(registration_year = year_supplier_registration) %>% 
   # Create or modify variables within each group
@@ -170,8 +170,8 @@ potential_suppliers_tech_balance <- potential_suppliers_tech_balance %>%
 
 
 # I create a matching year variable before matching to the orbis data
-potential_suppliers_tech_balance$matching_year<- potential_suppliers_tech_balance$registration_year  
-potential_suppliers_tech_balance_selected_countries<- potential_suppliers_tech_balance_selected_countries %>% 
+# potential_suppliers_tech_balance$matching_year<- potential_suppliers_tech_balance$registration_year  
+potential_suppliers_tech_balance_selected_countries<- potential_suppliers_tech_balance %>% 
   filter(country %in% c("ES", "FR", "IT", "GB")) %>% 
   filter(!is.na(bvd_id_number))
 
@@ -185,10 +185,84 @@ potential_suppliers_tech_balance_selected_countries_vars<- potential_suppliers_t
          matching_year = registration_year) %>% 
   ungroup() %>% 
   distinct() %>% 
-  select(bvd_id_number, country, registration_year, tech_level, well_balanced, supplies_ms_status) %>% 
+  select(bvd_id_number, country, registration_year, well_balanced,max_tech,  supplies_ms_status) %>% #I drop the tech level because I still have multiples 
   # I am dropping the x2_digit because I have multiple for the same company and year
-  distinct()
+  distinct() %>% 
+  rename(tech_level = max_tech)
   
+# I checked and all the bvds only have one year of registration
+
+
+## Merge orders and orbis data
+matched_potential_suppliers_orbis_data$matching_year <- matched_potential_suppliers_orbis_data$year# this creates the matching year
+matched_potential_suppliers_orbis_data<- matched_potential_suppliers_orbis_data %>% 
+  left_join(potential_suppliers_tech_balance_selected_countries_vars, by =c("bvd_id_number")) %>% 
+  filter(!is.na(registration_year)) # it seems that I have some companies that are potential suppliers for which I don't have the registration year
+
+matched_potential_suppliers_orbis_data<- matched_potential_suppliers_orbis_data %>% filter(!is.na(registration_year))
+
+# getting rid of all the variables that are not needed
+matched_potential_suppliers_orbis_data_vars<- matched_potential_suppliers_orbis_data %>% select( -identifier, -score, -nacerev2mainsection, 
+                                                                        -nacerev2mainsection, -nacerev2corecode4digits,-country_iso_code, 
+                                                                        -nacerev2secondarycodes,  -fax_number, -region_in_country,
+                                                                        -type_of_region_in_country,- street_no_building_etc_line_1,-street_no_building_etc_line_1_native, -street_no_building_etc_line_2, 
+                                                                        -street_no_building_etc_line_2_native, -street_no_building_etc_line_3, -street_no_building_etc_line_3_native) %>% distinct()
+
+
+
+
+saveRDS(matched_potential_suppliers_orbis_data_vars, paste0(data_proc_dir, "matched_potential_suppliers_orbis_data_vars"))
+
+# I create a new variables for the consolidations codes. Before doing this, I have U1, U2, C1, and C2. I want just the initial letters. 
+# Add the difference between consolidated and unconsolidated from the paper 
+matched_potential_suppliers_orbis_data_vars$consolidation_l <- substr(matched_potential_suppliers_orbis_data_vars$consolidation_code,1,1)
+matched_potential_suppliers_orbis_data_vars_unconsolidated <- matched_potential_suppliers_orbis_data_vars %>% 
+  filter(consolidation_l =="U") %>% distinct()
+
+matched_potential_suppliers_orbis_data_vars_unconsolidated<- matched_potential_suppliers_orbis_data_vars_unconsolidated %>% 
+  group_by(bvd_id_number) %>% 
+  mutate(first_year = min(year),# this gets the first year for which you have orbis data
+         registration_year = ifelse(!is.na(registration_year), registration_year, last(na.omit(registration_year))),
+         tech_level = ifelse(!is.na(tech_level), tech_level, last(na.omit(tech_level))),
+         last_year = max(year), # this gets the last year for which you have orbis data
+         registration_after_last_orbis = last_year<registration_year)# this says whether the last year for which I have data, is before the first order. 
+
+matched_potential_suppliers_orbis_data_vars_unconsolidated<- matched_potential_suppliers_orbis_data_vars_unconsolidated%>% 
+  group_by(bvd_id_number, year) %>% 
+  filter(ebitda == max(ebitda))%>% # if multiple ebitda per year, I get the maximum - I need to explain why I do this. 
+  select(-consolidation_code, -matched_company_name, -nacerev2primarycodes) %>% 
+  ungroup() %>% 
+  distinct()
+
+
+matched_potential_suppliers_orbis_data_vars_unconsolidated<- matched_potential_suppliers_orbis_data_vars_unconsolidated %>%
+  group_by(bvd_id_number) %>% 
+  mutate(year_after_order = last_year - registration_year, # This calculates how many years you have after the registration
+         year_before_order = registration_year -first_year) %>%  # This calculates how many years before the registration. 
+  ungroup() %>% 
+  distinct() 
+
+matched_potential_suppliers_orbis_data_vars_unconsolidated<- matched_potential_suppliers_orbis_data_vars_unconsolidated %>% 
+  select(-city, - closing_date, -street_no_building_etc_line_4, -street_no_building_etc_line_4_native, -postcode, 
+         -city_native, -telephone_number, -address_type, -date_closing, -closing_date_format) %>% distinct() # I Am getting rid of the variables that I don't need
+
+saveRDS(matched_potential_suppliers_orbis_data_vars_unconsolidated, paste0(data_proc_dir, "matched_potential_suppliers_orbis_data_vars_unconsolidated"))
+
+# Create lookup for incorporation, size, nace and activity 
+## this is then used to create the incorporation_nace_size_data
+
+incorporation_size_lookup <- matched_potential_suppliers_orbis_data_vars_unconsolidated %>% 
+  select(bvd_id_number) %>% distinct()
+split_and_write_csv(incorporation_size_lookup, 725, incorporation_size_nace_lookup_pot_dir)
+
+
+#Create the lookup for patents
+
+patent_bvd_lookup <-matched_potential_suppliers_orbis_data_vars_unconsolidated  %>% 
+  select(bvd_id_number) %>% distinct()
+
+split_and_write_csv(patent_bvd_lookup, 100, pot_supplier_patent_lookup_dir)
+
 
 
 all_orders_tech_balance_selected_countries_vars <- all_orders_tech_balance_selected_countries %>% 
